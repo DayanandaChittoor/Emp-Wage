cf79cb60ec3781eeca9ea9968cb37e27a9ad2881
DayanandaChittoor <seenu.daya5@gmail.com>
2021-07-24 18:29:10 +0530

    Added: Employee Part Time along with Wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,7 +2,6 @@
 
 #constant
 IS_FULL_TIME=1
-IS_PART_TIME=2
 EMP_RATE_PER_HR=20
 
 #variable
@@ -14,11 +13,14 @@ if [ $empCheck -ne 0 ]
 then
 	if [ $empCheck -eq $IS_FULL_TIME ]
 		then
+			echo "Employee Presnt for Full Time"
 			empHrs=16
 	else
+		echo "Employee Present for Part Time"
 		empHrs=8
 	fi
 else
+	echo "Employee is Absent"
 	wage=0
 fi
 
