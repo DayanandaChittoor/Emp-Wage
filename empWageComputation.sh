762e1c8ad385d1a51af0a17d9391bfbb46332bd9
DayanandaChittoor <seenu.daya5@gmail.com>
2021-07-27 16:21:13 +0530

    Added store daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,11 +5,12 @@ IS_FULL_TIME=1
 IS_PART_TIME=2
 EMP_RATE_PER_HR=20
 WORKING_DAYS=30
-MAX_HRS_MONTH=20
+MAX_HRS_MONTH=140
 
 #variable
 totalWorkingDays=0
 totalEmpHrs=0
+day=0
 
 function workHours() {
 
@@ -21,7 +22,7 @@ case $empCheck in
 		empHrs=4
 		;;
 	*)
-		empHrs=00
+		empHrs=0
 		;;
 esac
 
@@ -30,10 +31,15 @@ esac
 while [ $totalEmpHrs -lt $MAX_HRS_MONTH ] && [ $totalWorkingDays -lt $WORKING_DAYS ]
 do 
 	workHours $((RANDOM%3))
-totalEmpHrs=$(($totalEmpHrs+$empHrs))
+	totalEmpHrs=$(($totalEmpHrs+$empHrs))
 	((totalWorkingDays++))
+	todaysWage=$((totalEmpHrs*$EMP_RATE_PER_HR))
+	dayWage[((day++))]=$todaysWage
 done
 
+
 totalSalary=$((totalEmpHrs*$EMP_RATE_PER_HR))
-echo $totalSalary
-echo $totalEmpHrs
+echo ${dayWage[@]}
+echo "Total Wage is : " $totalSalary
+echo "Total working Hours : " $totalEmpHrs
+
